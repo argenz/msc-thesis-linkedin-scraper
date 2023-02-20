2d0fb2fdffe32632e9bb7af4d9a5532257c52498
Francesca Argenziano <47746078+argenz@users.noreply.github.com>
2023-02-20 21:58:13 +0100

    Update ab_utils.py

--- ABW-PublicIDs-Docker-Deployer/ab_utils.py
+++ ABW-PublicIDs-Docker-Deployer/ab_utils.py
@@ -13,12 +13,12 @@ end = {end}
     log.info(f'Written new indexes.py file with start: {start}, end {end}.')
 
 #
-def write_config_py(file_path_name, email):
+def write_config_py(file_path_name, email, pwd):
     with open(file_path_name, 'w') as f:
 
         f.write(f'''\
 email = "{email}"
-pwd = "LinkedinMember.1998"     
+pwd = "{pwd}"     
 ''')
 
     log.info(f'Written new config.py with email {email}')
